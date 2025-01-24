3a3aed77dcc43a564b67378a1db32861cd50d478
leeseunghee00 <lsh9246@gmail.com>
2025-01-24 23:04:03 +0900

    initialize

---
+++ src/main/java/com/leeseunghee/study/ConcurrentStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.leeseunghee.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentStockManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeseunghee/study/ConcurrentStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeseunghee.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentStockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
