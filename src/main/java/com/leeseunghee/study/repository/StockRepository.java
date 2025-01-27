067f2caedca5432a10e71d373f6cacd6dd14c1bb
leeseunghee00 <lsh9246@gmail.com>
2025-01-27 14:35:47 +0900

    feat: Pessmistic Lock 적용 및 테스트

--- src/main/java/com/leeseunghee/study/repository/StockRepository.java
+++ src/main/java/com/leeseunghee/study/repository/StockRepository.java
@@ -1,8 +1,16 @@
 package com.leeseunghee.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.leeseunghee.study.domain.Stock;
 
+import jakarta.persistence.LockModeType;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT s FROM Stock s WHERE s.id = :id")
+	Stock findByWithIdPessimisticLock(Long id);
 }
---
+++ src/test/java/com/leeseunghee/study/service/PessimisticLockStockServiceTest.java
@@ -0,0 +1,62 @@
+package com.leeseunghee.study.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.leeseunghee.study.domain.Stock;
+import com.leeseunghee.study.repository.StockRepository;
+
+@SpringBootTest
+class PessimisticLockStockServiceTest {
+
+	@Autowired
+	private PessimisticLockStockService pessimisticLockStockService;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@BeforeEach
+	public void before() {
+		stockRepository.save(new Stock(1L, 100L));
+	}
+
+	@AfterEach
+	public void after() {
+		stockRepository.deleteAll();
+	}
+
+	@Test
+	public void 동시_100개_요청_pessimistic_lock() throws InterruptedException {
+		// given -- 테스트의 상태 설정
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		// when -- 테스트하고자 하는 행동
+		for (int i = 0; i < threadCount; i++) {
+			executorService.execute(() -> {
+				try {
+					pessimisticLockStockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		// then -- 예상되는 변화 및 결과
+		assertEquals(0, stock.getQuantity());
+	}
+}
